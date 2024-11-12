8ad47b09a102d2375d6465ae440e99dac6cfba1f
archijs24 <archanasalunke137@gmail.com>
2024-11-12 12:57:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+<html>
+	<head>
+	<title>Anagram Checker</title>
+	</head>
+	<style>
+	</style>
+	<body>
+	<center>
+		<h1>ANAGRAM CHECKER</h1>
+		<form onsubmit="check()">
+		<input type="text" id="string1" placeholder="enter first string">
+		<br><br>
+		<input type="text" id="string2" placeholder="enter second string">
+		<br><br>
+		<input type="submit" value="check">
+	</center>
+	</body>
+</html>
